20eca49943e0b40f4e85c8e3370385ace3869c88
Leo <37898564+sysuleo@users.noreply.github.com>
2019-02-28 17:39:38 +0800

    Create （剑指offer）判断栈弹出合法性.cpp

---
+++ （剑指offer）判断栈弹出合法性.cpp
@@ -0,0 +1,46 @@
+/*
+题目描述
+输入两个整数序列，第一个序列表示栈的压入顺序，请判断第二个序列是否可能为该栈的弹出顺序。假设压入栈的所有数字均不相等。例如序列1,2,3,4,5是某栈的压入顺序，序列4,5,3,2,1是该压栈序列对应的一个弹出序列，但4,3,5,1,2就不可能是该压栈序列的弹出序列。（注意：这两个序列的长度是相等的）
+*/
+class Solution {
+public:
+    bool IsPopOrder(vector<int> pushV,vector<int> popV) {
+        stack<int>res;
+        if(pushV.size()!=popV.size()) return false;
+        int i=0;
+        int j=0;
+        while(i<pushV.size()){
+            res.push(pushV[i]);
+            i++;
+            if(res.top()==popV[j]){
+                res.pop();
+                j++;
+            }
+        }
+        while(j<popV.size()){  
+            if(popV[j]!=res.top()){
+                return false;
+            }
+            else{
+                res.pop();
+                j++;
+            }
+        }
+    }
+};
+
+class Solution {
+public:
+    bool IsPopOrder(vector<int> pushV,vector<int> popV) {
+        vector<int>stack;
+        if(pushV.size()!=popV.size()) return false;
+        for(int i=0,j=0;i<pushV.size();){
+            stack.push_back(pushV[i++]);
+            while(j < popV.size() && stack.back() == popV[j]){
+                stack.pop_back();
+                j++;
+            }
+        }
+        return stack.empty();
+    }
+};
