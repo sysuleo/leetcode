f2c96fdc7715a7c1555302fa502d4ba552810d6a
Leo <37898564+sysuleo@users.noreply.github.com>
2019-06-16 00:39:58 -0700

    Update 36. Valid Sudoku.cpp

--- 36. Valid Sudoku.cpp
+++ 36. Valid Sudoku.cpp
@@ -1,3 +1,4 @@
+/*
 Determine if a 9x9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
 Each row must contain the digits 1-9 without repetition.
 Each column must contain the digits 1-9 without repetition.
@@ -35,3 +36,9 @@ Input:
 Output: false
 Explanation: Same as Example 1, except with the 5 in the top left corner being 
     modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+*/
+/*
+coding 期间遇到多维vector初始化问题
+https://github.com/sysuleo/leetcode/blob/master/leetcode/多维vector的初始化.md
+*/
+
