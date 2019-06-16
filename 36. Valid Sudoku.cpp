8b0a6d0bb203b587c964ae7217389af3a2a716b8
Leo <37898564+sysuleo@users.noreply.github.com>
2019-06-16 00:47:29 -0700

    Update 36. Valid Sudoku.cpp

--- 36. Valid Sudoku.cpp
+++ 36. Valid Sudoku.cpp
@@ -37,8 +37,34 @@ Output: false
 Explanation: Same as Example 1, except with the 5 in the top left corner being 
     modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
 */
+
+
 /*
 coding 期间遇到多维vector初始化问题
 https://github.com/sysuleo/leetcode/blob/master/leetcode/多维vector的初始化.md
 */
 
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        vector<vector<bool>> col(9,vector<bool>(10,false));//初始化一个二维bool型vector
+        vector<vector<bool>> row(9,vector<bool>(10,false));
+        vector<vector<bool>> grid(9,vector<bool>(10,false)); //由于数字是1-9，因此每一维初始化10个false
+        for(int i=0;i<9;i++){ //row index
+            for(int j=0;j<9;j++){  //col index
+                if(board[i][j]=='.') continue;
+                int num=board[i][j]-'0';
+                if(row[i][num]==false) row[i][num]=true; //num didn't appeared in row 'i',mark it
+                else return false; //num has appeared in row 'i',so reture false
+                
+                if(col[j][num]==false) col[j][num]=true; //num didn't appeared in col 'j',mark it
+                else return false;
+                
+                int gridindex=(i/3)*3+(j/3); //向下取整
+                if(grid[gridindex][num]==false) grid[gridindex][num]=true; //num didn't appeared in grid 'gridindex',mark it
+                else return false;
+            }
+        }
+        return true;
+    }
+};
